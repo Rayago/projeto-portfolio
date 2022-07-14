fbc87380c82dfaca24cef94f04d71056d7ecb112
Raymundo Thiago <raymundothiago@hotmail.com>
2022-07-13 23:52:34 -0300

    :sparkles: Adicionar seção de habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,16 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace("bi-x", "bi-list")
     body.classList.toggle("menu-nav-active")
 });
+
+/*Fecha menu quando selecionar opção*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
